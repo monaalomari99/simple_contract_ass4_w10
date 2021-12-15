571bdf09f2d0217bed66964c5e99a2bfcd164deb
monaalomari99 <92031414+monaalomari99@users.noreply.github.com>
2021-12-15 16:29:40 +0300

    first commit

---
+++ Bank.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.19;
+contract Bank {
+
+    mapping(address => uint256) public balanceOf;   // balances, indexed by addresses
+
+    function deposit(uint256 amount) public payable {
+        require(msg.value == amount);
+        balanceOf[msg.sender] += amount;     // adjust the account's balance
+    }
+
+    function withdraw(uint256 amount) public {
+        require(amount <= balanceOf[msg.sender]);
+        balanceOf[msg.sender] -= amount;
+        msg.sender.transfer(amount);
+    }
+}
---
+++ Empty.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.19;
+
+contract empty {
+
+    
+
+    
+}
+
